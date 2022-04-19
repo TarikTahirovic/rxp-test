53b3ba407e4e5af23e5f8214490e56eef00a0605
Tarik Tahirovic <tariktahirovic@rezolve.com>
2022-04-19 15:52:05 +0200

    test2

--- apps/rezolve/src/app/app.module.ts
+++ apps/rezolve/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { TesticonComponent } from './testicon/testicon.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent, NxWelcomeComponent, TesticonComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
