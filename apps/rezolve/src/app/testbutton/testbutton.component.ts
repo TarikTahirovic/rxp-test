cfb9f46173834006ec65c975fa66efe3580b6925
Tarik Tahirovic <tariktahirovic@rezolve.com>
2022-04-19 13:40:31 +0200

    test

---
+++ apps/rezolve/src/app/testbutton/testbutton.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'rxp-test-testbutton',
+  templateUrl: './testbutton.component.html',
+  styleUrls: ['./testbutton.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated,
+})
+export class TestbuttonComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
